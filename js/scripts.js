5ae09402ffc8c59d137f5b0047dc800c138d6a98
Carson Canales and Ryan Nunez <ryan@>
2017-07-19 16:51:07 -0700

    add constructor

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,29 @@ var rollDice = function(){
   return Math.floor(Math.random() * 6) + 1;
 
 }
-var player_score =
+function Player(name,score,turn){
+  this.name = name;
+  this.score = 0;
+  this.turn = 0;
+}
 
+
+var newPlayer = new Player();
+
+// Player.prototype.name = function() {
+//       if() {
+//         return ;
+//       } else {
+//         return  ;
+//       }
+//     }
 
 $(document).ready(function(){
+  $("form#player").submit(function(event) {
+    var inputtedName = $("input#player").val();
+
+})
+
   $("#rollButton").click(function() {
 
     var roll= rollDice
