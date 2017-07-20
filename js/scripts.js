8c7b1ade27b84c5dd5e22e696ae7a714a763e802
Carson Canales and Ryan Nunez <ryan@>
2017-07-20 10:55:31 -0700

    reworked constructors and prototypes. also worked on the output

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,63 @@
-// // Buisness logic
 var rollDice = function(){
   return Math.floor(Math.random() * 6) + 1;
 
 }
-function Player(name,score,turn){
-  this.name = name;
-  this.score = 0;
-  this.turn = 0;
-}
 
-
-var newPlayer = new Player();
-
-// Player.prototype.name = function() {
-//       if() {
-//         return ;
-//       } else {
-//         return  ;
-//       }
-//     }
 
 $(document).ready(function(){
-  $("form#player").submit(function(event) {
+
+  // U/I logic
+  $("form.form-inline").submit(function(event) {
+    event.preventDefault();
     var inputtedName = $("input#player").val();
+    var addPlayer = new Player(inputtedName);
 
-})
 
+
+    // Buisness logic
+
+
+   function Player(name,score,turn){
+     this.name = name;
+     this.score = score;
+     this.turn = turn;
+   }
+
+   Player.prototype.newPlayer = function() {
+     return 0 + this.Name + this.Score + this.Turn;
+   }
+
+   Player.prototype.Name = function() {
+     return inputtedName;
+     }
+   // Player.prototype.Score = function() {
+   //   if(|| ) {
+   //     return ;
+   //   } else {
+   //     return  ;
+   //   }
+   // }Player.prototype.Turn = function() {
+   //   if( || ) {
+   //     return ;
+   //   } else {
+   //     return  ;
+   //   }
+   // }
+
+    var inputtedName = " ";
+    var score = 0;
+    var turn = 0;
+
+     $("ul#player1name").append("<span class='player_1'>" + addPlayer.Name + "</span>");
+
+    //  $("input#player").val("");
+
+  })
   $("#rollButton").click(function() {
 
     var roll= rollDice
     $("span#die1").text(roll);
     $("span#die2").text(roll);
-      event.preventDefault();
+
   });
 });
