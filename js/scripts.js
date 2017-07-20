4803b01de87b4c9bebeda682fd7c0b490a7c050e
Carson Canales and Ryan Nunez <carson+ryan@>
2017-07-20 16:26:01 -0700

    Include number 1 rule exception statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
 var rollDice = function(){
-  return Math.floor(Math.random() * 6) + 1;
+  var diceRoll = Math.floor(Math.random() * 6) + 1;
+  if (diceRoll === 1){
+    // this.Turn = 0;
+    alert("Know when to quit when your ahead! you rolled a 1 your turn is over and you gain no points for this round.");
+  }
+  return diceRoll;
+  // else if{
+  //   ((die1 + die2) > 2 || (die3 + die4) > 2){
+  //     return
+  //   }
+  //
+  // }
+
 
 }
 
@@ -26,28 +38,28 @@ $(document).ready(function(){
      this.turn = turn
    }
     var addPlayer = new Player(inputtedName,0,0);
-    console.log(addPlayer.name);
-    console.log(addPlayer.score);
-    console.log(addPlayer.turn);
+    // console.log(addPlayer.name);
+    // console.log(addPlayer.score);
+    // console.log(addPlayer.turn);
 
     var addPlayer2 = new Player(inputtedName2,0,0);
-    console.log(addPlayer2.name);
-    console.log(addPlayer2.score);
-    console.log(addPlayer2.turn);
+    // console.log(addPlayer2.name);
+    // console.log(addPlayer2.score);
+    // console.log(addPlayer2.turn);
 
-   // Player.prototype.Score = function() {
-   //   if(|| ) {
-   //     return ;
-   //   } else {
-   //     return  ;
-   //   }
-   // }Player.prototype.Turn = function() {
-   //   if( || ) {
-   //     return ;
-   //   } else {
-   //     return  ;
-   //   }
-   // }
+  //  Player.prototype.Score = function() {
+  //    if(|| ) {
+  //      return ;
+  //    } else {
+  //      return  ;
+  //    }
+  //  }Player.prototype.Turn = function() {
+  //    if( || ) {
+  //      return ;
+  //    } else {
+  //      return  ;
+  //    }
+  //  }
    Player.prototype.start = function() {
    return this.name + " " + this.score + " " + this.turn+ " ";
    }
@@ -61,25 +73,26 @@ $(document).ready(function(){
 
      $("ul#player1name").append("<span class='player_1'>" + addPlayer.name + "</span>");
      $("ul#player1score").append("<span class='player_1'>" + addPlayer.score + "</span>");
-     $("ul#player1turn").append("<span class='player_1'>" + addPlayer.score + "</span>");
+     $("ul#player1turn").append("<span class='player_1'>" + addPlayer.turn + "</span>");
      $("ul#player2name").append("<span class='player_1'>" + addPlayer2.name + "</span>");
      $("ul#player2score").append("<span class='player_1'>" + addPlayer2.score + "</span>");
-     $("ul#player2turn").append("<span class='player_1'>" + addPlayer2.score + "</span>");
+     $("ul#player2turn").append("<span class='player_1'>" + addPlayer2.turn + "</span>");
 
     //  $("input#player").val("");
 
   })
   $("#rollButton").click(function() {
 
-    var roll= rollDice
+    var roll= rollDice();
+    var roll1= rollDice();
     $("span#die1").text(roll);
-    $("span#die2").text(roll);
+    $("span#die2").text(roll1);
 
 
   });
   $("#rollButton2").click(function() {
 
-    var roll= rollDice
+    var roll= rollDice();
     $("span#die3").text(roll);
     $("span#die4").text(roll);
 
